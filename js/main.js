36e36b270b99b9d1aee8fb43949822015ab28353
Matteo Basile <matteo.basile56@gmail.com>
2021-01-12 22:25:54 +0100

    es completo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$( ".header-right " ).click(function() {
+  $( ".hamburger-menu" ).show( "slow", function() {
+  });
+});
+$( ".close" ).click(function() {
+  $( ".hamburger-menu" ).hide( "slow", function() {
+  });
+});
